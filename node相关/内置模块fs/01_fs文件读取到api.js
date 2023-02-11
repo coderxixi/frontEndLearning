d813a227c9b3a16301f711401ddb145fa8ee6827
liuss7 <976344695@qq.com>
2023-02-11 13:59:19 +0800

    异步读取 回调函数

--- node相关/内置模块fs/01_fs文件读取到api.js
+++ node相关/内置模块fs/01_fs文件读取到api.js
@@ -1,11 +1,15 @@
 const fs  = require("fs");
 //  同步读取
-const res1= fs.readFileSync('./abc.txt','utf-8');
+const res1= fs.readFileSync('./abc.txt',{encoding:'utf-8'});
 console.log('res1',res1)
 
 
 
 // 异步读取 回调函数
+
+fs.readFile('./abc.txt',{encoding:'utf-8'},(err,data)=>{
+  console.log('res',data);
+})
 
 
 
